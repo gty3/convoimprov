6e13ad9e67a085675fef092442815e22f8437da5
gty <gefyoung@gmail.com>
2023-04-26 16:05:26 +1000

    messing with mobile&desktop shit

--- app/components/chatComponent.tsx
+++ app/components/chatComponent.tsx
@@ -24,35 +24,6 @@ export default function ChatComponent({
 
   const messageRef = useRef<any>(null)
 
-  const ChatInput = () => {
-    return (
-      <div className="justify-center hidden sm:hidden md:flex md:mt-20">
-        <textarea
-          autoFocus
-          autoComplete="off"
-          rows={4}
-          className="px-1 border border-black w-80 dark:text-black"
-          onChange={(e) => handleTextChange(e)}
-          ref={messageRef}
-        />
-      </div>
-    )
-  }
-  const ChatInputMobile = () => {
-    return (
-      <div className="absolute bottom-0 flex justify-center md:hidden">
-        <input
-          autoFocus
-          autoComplete="off"
-          // rows={4}
-          className="px-1 border border-black w-80 dark:text-black"
-          onChange={(e) => handleTextChange(e)}
-          ref={messageRef}
-        />
-      </div>
-    )
-  }
-
   const messageDebounced = debounce(async () => {
     if (!messageRef.current) return
     if (!session) return
@@ -107,47 +78,100 @@ export default function ChatComponent({
     })
   }, [session, token])
 
-  return (
+  const DesktopComponent = () => {
+    return (
+      <div className="h-96">
+        {/* <div className="w-96"> */}
+        <div className="absolute flex justify-center pt-2 bottom-20 md:relative md:pt-10">
+          <div className="relative opacity-100 w-80 h-80">
+            <Image
+              src="/cafe_eating_dark.jpg"
+              fill={true}
+              alt="guy eating at cafe"
+              className="flex items-center justify-center border border-black"
+            />
+            <div className="p-2 z-1 text-lg text-white drop-shadow-[2px_2px_2px_rgba(0,0,0,0.8)]">
+              {/* Hey hows it going, this is default tttt text, my name is geoff, lol that always gets a chuckle - my name is geoff. Im sitting here eating */}
+              {receivedText}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="p-1.5 md:py-2 text-lg w-80 h-16">
+            {/* Hi I'm a stranger looking to talk, ttttt this is random default text */}
+            {sentText}
+          </div>
+        </div>
+        <div className="justify-center hidden sm:hidden md:flex md:mt-20">
+          <textarea
+            autoFocus
+            autoComplete="off"
+            rows={4}
+            className="px-1 border border-black w-80 dark:text-black"
+            onChange={(e) => handleTextChange(e)}
+            ref={messageRef}
+          />
+        </div>
+        {/* </div> */}
+      </div>
+    )
+  }
+
+  const MobileComponent = () => {
+    return (
+      <div className="h-96 w-96">
+        <div className="absolute inset-x-0 bottom-20 w-96">
+          <div className="flex justify-center pt-2">
+            <div className="relative w-60 h-60">
+              <Image
+                src="/cafe_eating_dark.jpg"
+                fill={true}
+                alt="guy eating at cafe"
+                className="flex items-center justify-center border border-black"
+              />
+              <div className="p-2 z-1 text-lg text-white drop-shadow-[2px_2px_2px_rgba(0,0,0,0.8)]">
+                {/* Hey hows it going, this is default tttt text, my name is geoff, lol that always gets a chuckle - my name is geoff. Im sitting here eating */}
+                {receivedText}
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="p-1.5 text-lg w-80 h-16">
+              {/* Hi I'm a stranger looking to talk, ttttt this is random default text */}
+              {sentText}
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <input
+              // autoFocus
+              autoComplete="off"
+              className="px-1 border border-black w-80 dark:text-black"
+              onChange={(e) => handleTextChange(e)}
+              ref={messageRef}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return !adminConnected ? (
     <>
-      <div className="h-screen overflow-hidden">
-        {adminConnected ? (
-          <div className="flex justify-center ">
-            <div className="w-96">
-              <div className="flex justify-center pt-2 md:pt-10">
-                <div className="relative opacity-100 w-60 h-60 md:w-80 md:h-80">
-                  <Image
-                    src="/cafe_eating_dark.jpg"
-                    fill={true}
-                    alt="guy eating at cafe"
-                    className="flex items-center justify-center border border-black"
-                  />
-                  <div className="p-2 z-1 text-lg absolute bottom-0 text-white drop-shadow-[2px_2px_2px_rgba(0,0,0,0.8)]">
-                    {/* Hey hows it going, this is default tttt text, my name is geoff, lol that always gets a chuckle - my name is geoff. Im sitting here eating */}
-                    {receivedText}
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <div className="p-1.5 md:py-2 text-lg w-80 h-16">
-                  {/* Hi I'm a stranger looking to talk, ttttt this is random default text */}
-                  {sentText}
-                </div>
-              </div>
-              <ChatInput />
-              <ChatInputMobile />
-            </div>
-          </div>
-        ) : (
-          <div className="flex justify-center pt-64">
-            <div className="grid grid-cols">
-              <CustomSpinner className="flex justify-center" />
-              <div className="flex justify-center px-4 pt-6">
-                Connecting. This can take a minute
-              </div>
-            </div>
-          </div>
-        )}
+      <div className="md:hidden">
+        <MobileComponent />
+      </div>
+      <div className="hidden md:flex">
+        <DesktopComponent />
       </div>
     </>
+  ) : (
+    <div className="flex justify-center pt-64">
+      <div className="grid grid-cols">
+        <CustomSpinner className="flex justify-center" />
+        <div className="flex justify-center px-4 pt-6">
+          Connecting. This can take a minute
+        </div>
+      </div>
+    </div>
   )
 }
