89ccd90a4513a00df61912eb7ea0bdab3b87c242
gty <gefyoung@gmail.com>
2023-04-26 12:53:38 +1000

    not console logging in prod server

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import ApproachCard from "./approachCard"
 
 export default function Home() {
+  console.log('hello, does this get logged?')
+  console.log(process.env)
   const EmailButton = () => {
     return (
       <Link
