89197be92bb80371a573e6794bdbc9fb41a42026
gty <gefyoung@gmail.com>
2023-04-26 12:48:09 +1000

    added ses, should be prod only

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,13 +1,13 @@
 import ChatComponent from "../components/chatComponent"
-// import { SESClient, CloneReceiptRuleSetCommand } from "@aws-sdk/client-ses"
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses"
 import OpenTok from "opentok"
 
 export default async function ChatPage() {
-  // const client = new SESClient({ region: "ap-southeast-2" })
   const opentok = new OpenTok(
     process.env.NEXT_PUBLIC_OPENTOK_APIKEY,
     process.env.OPENTOK_SECRET
   )
+  console.log("NODE ENV:::", process.env.NODE_ENV)
 
   function createSession(): Promise<OpenTok.Session> {
     return new Promise(function (resolve, reject) {
@@ -23,6 +23,31 @@ export default async function ChatPage() {
 
   const session = await createSession()
   const sessionId = session.sessionId
+  if (process.env.NODE_ENV !== "development") {
+    try {
+      await new SESClient({ region: "us-east-1" }).send(
+        new SendEmailCommand({
+          Source: "gefyoung@gmail.com",
+          Destination: {
+            ToAddresses: ["gefyoung@gmail.com"]
+          },
+          Message: {
+            Subject: {
+              Data: "convoImprov Call"
+            },
+            Body: {
+              Text: {
+                Data: "https://convoimprov.vercel.app/admin/" + sessionId
+              }
+            }
+          }
+        })
+      )
+    } catch (err) {
+      console.log('SES-ERR:::', err)
+    }
+  }
+
   const token = opentok.generateToken(sessionId)
 
   return (
