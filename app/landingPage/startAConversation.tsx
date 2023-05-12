85c8230df3bb69f8a35bd7c1cd402958b1ecb75e
gty <gefyoung@gmail.com>
2023-05-12 11:08:34 +1000

    why isnt email workingin rpod

--- app/landingPage/startAConversation.tsx
+++ app/landingPage/startAConversation.tsx
@@ -23,31 +23,38 @@ export default function StartAConversation() {
 
   const startClicked = async () => {
     setLookingState(true)
-    const fetchAdmin = await fetch("/api/fetchAdmin")
-    const res = await fetchAdmin.json()
-    const { sessionId, token } = res
+    try {
+      const fetchAdmin = await fetch("/api/fetchAdmin")
+      const res = await fetchAdmin.json()
+      const { sessionId, token } = res
+      console.log('res:', res)
+  
+      const OT = (await import("@opentok/client")).default
+        const otSession = OT.initSession(
+          process.env.NEXT_PUBLIC_OPENTOK_APIKEY,
+          sessionId
+        )
+        // console.log("otSession::", otSession)
+        otSession.connect(token, function (err) {
+          if (err) {
+            console.log("session-connect-error:::", err)
+          }
+        })
+        otSession.on("connectionCreated", (event) => {
+          // console.log("CONNECTION!!!@@@@@@@")
+          // router.push(`/chat/${sessionId}`)
+          const eventConnectionId = event.connection.connectionId
+          const thisConnectionId = otSession.connection?.connectionId
+          if (eventConnectionId !== thisConnectionId) {
+            router.push(`/chat/${sessionId}`)
+          }
+        })
+  
+      // timerRef.current(sessionId)
+    } catch (err) {
+      console.log("err:", err)
+    }
 
-    const OT = (await import("@opentok/client")).default
-      const otSession = OT.initSession(
-        process.env.NEXT_PUBLIC_OPENTOK_APIKEY,
-        sessionId
-      )
-
-      // otSession.connect(token, function (err) {
-      //   if (err) {
-      //     console.log("session-connect-error:::", err)
-      //   }
-      // })
-      otSession.on("connectionCreated", (event) => {
-        router.push(`/chat/${sessionId}`)
-        // const eventConnectionId = event.connection.connectionId
-        // const thisConnectionId = otSession.connection?.connectionId
-        // if (eventConnectionId !== thisConnectionId) {
-        //   router.push(`/chat/${sessionId}`)
-        // }
-      })
-
-    // timerRef.current(sessionId)
   }
   const closeLooking = async () => {
     // clearTimeout(timerRef.current)
