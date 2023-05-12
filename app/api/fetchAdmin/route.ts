63fd585aa54b0b6618200ee14757c513896fc2bb
gty <gefyoung@gmail.com>
2023-05-12 12:16:51 +1000

    no sessionId in email

--- app/api/fetchAdmin/route.ts
+++ app/api/fetchAdmin/route.ts
@@ -43,7 +43,7 @@ export async function GET() {
             },
             Body: {
               Text: {
-                Data: "https://convoimprov.vercel.app/admin/" + sessionId,
+                Data: "https://convoimprov.vercel.app/admin/",
               },
             },
           },
